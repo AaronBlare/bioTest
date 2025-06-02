ebbe04a055b97cfb6956e7f9ba238b39d39e82c4
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2025-06-02 21:50:24 +0300

    test geo analyzing

---
+++ test_mliftover.R
@@ -0,0 +1,87 @@
+rm(list=ls())
+
+###############################################
+# Installing packages
+###############################################
+if (!requireNamespace("BiocManager", quietly=TRUE))
+  install.packages("BiocManager")
+BiocManager::install("ChAMP")
+library("ChAMP")
+
+###############################################
+# Setting variables
+###############################################
+dataset_450 <- 'GSE87571'
+arraytype_450 <- '450K'
+
+dataset_EPIC <- 'GSE234461'
+arraytype_EPIC <- 'EPIC'
+
+###############################################
+# Setting path
+# Directory must contain *.idat files and *.csv file with phenotype
+###############################################
+path_data_450 <- "E:/YandexDisk/DNAm draft/GEO/GPL13534/24_GSE87571/raw"
+path_data_EPIC <- "E:/YandexDisk/DNAm draft/GEO/GPL21145/93_GSE234461/raw"
+
+path_work <- path_data_450
+setwd(path_work)
+
+###############################################
+# Import and filtration
+###############################################
+myLoad_450 <- champ.load(
+  directory = path_data_450,
+  arraytype = arraytype_450,
+  method = "minfi",
+  methValue = "B",
+  autoimpute = TRUE,
+  filterDetP = TRUE,
+  ProbeCutoff = 0.1,
+  SampleCutoff = 0.1,
+  detPcut = 0.01,
+  filterBeads = FALSE,
+  beadCutoff = 0.05,
+  filterNoCG = FALSE,
+  filterSNPs = FALSE,
+  filterMultiHit = FALSE,
+  filterXY = FALSE,
+  force = TRUE
+)
+pd_450 <-  as.data.frame(myLoad_450$pd)
+
+###############################################
+# Functional normalization
+###############################################
+betas_450 <- getBeta(preprocessFunnorm(myLoad_450$rgSet))
+
+path_work <- path_data_EPIC
+setwd(path_work)
+
+###############################################
+# Import and filtration
+###############################################
+myLoad_EPIC <- champ.load(
+  directory = path_data_EPIC,
+  arraytype = arraytype_EPIC,
+  method = "minfi",
+  methValue = "B",
+  autoimpute = TRUE,
+  filterDetP = TRUE,
+  ProbeCutoff = 0.1,
+  SampleCutoff = 0.1,
+  detPcut = 0.01,
+  filterBeads = FALSE,
+  beadCutoff = 0.05,
+  filterNoCG = FALSE,
+  filterSNPs = FALSE,
+  filterMultiHit = FALSE,
+  filterXY = FALSE,
+  force = TRUE
+)
+pd_EPIC <-  as.data.frame(myLoad_EPIC$pd)
+
+###############################################
+# Functional normalization
+###############################################
+betas_EPIC <- getBeta(preprocessFunnorm(myLoad_EPIC$rgSet))
